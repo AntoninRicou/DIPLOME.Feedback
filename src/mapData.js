84d31f7dc393c1c452c31f2c227784a9be2c0e36
Anto44 <antoninricou96@gmail.com>
2026-05-24 14:50:50 +0200

    feat : align relational rendering with Euclidean proximity model

--- src/mapData.js
+++ src/mapData.js
@@ -3,7 +3,7 @@ const sources = {
     projection_2d: '/data/projection_2d.json',
     umap_book: '/data/umap_book2.json',
     umap_subjects_embeddings: '/data/umap_subjects_embeddings2.json',
-    umap_random: '/data/umap_random2.json',
+    umap_random: '/data/umap_replay.json',
 };
 
 async function loadMapData(mapType) {
